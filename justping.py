ffcc2b35cef7c208f03982e0e29cffe1984bcc2f
Mozillazg <opensource.mozillazg@gmail.com>
2012-10-26 13:05:29 +0800

    增加对 Linux，Unix 的支持，理论上支持 Mac

--- justping.py
+++ justping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """通过 ping 命令找出响应时间最快的 ip 或域名
-只支持 windows
+支持 windows，uinx，linux
 """
 
 import subprocess
@@ -12,30 +12,37 @@ def ping(host):
     host 参数应为字符串类型的 ip 或域名
         '192.168.1.1' or 'www.baidu.com'
     返回 host, ip, time, lost
-        host：域名，字符串类型
-        ip：字符串类型，默认值为'0.0.0.0'
+        host：域名或 ip，字符串类型
+        ip：ip 地址，字符串类型，默认值为'0.0.0.0'
         time：平均响应时间（ms），int 类型，默认值为0
         lost：平均丢包率（%），int 类型，默认值为0
     返回值示例：
-        ('www.baidu.com', '0.0.0.0', 0, 0)
+        ('baidu.com', '123.125.114.144', 70, 0)
     """
     import re
-    cmd = 'ping ' + host
+    import os
+    os_name = os.name
+    # 根据系统平台设置 ping 命令
+    if os_name == 'nt':  # windows
+        cmd = 'ping ' + host
+    else:  # unix/linux
+        cmd = 'ping -c r ' + host
     # 执行 ping 命令，并获取命令执行结果
     sub = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     out = sub.communicate()[0]
     # 替换换行符，因为在正则表达式中
     # 'a$' 匹配 'a\r\n' 中的 'a\r'
-    text = out.replace('\r\n', '\n')
-    ip = '0.0.0.0'
-    lost, time = 0, 0
-    try:
-        # 使用正则表达式提取信息
-        ip = re.search(r'^\d+\.\d+\.\d+\.\d+\b', text, re.M).group()
-        time = int(re.search(r'\d+(?=ms$)', text).group())
-        lost = int(re.search(r'\d+(?=%)', text).group())
-    except:
-        pass
+    text = out.replace('\r\n', '\n').replace('\r', '\n')
+    # 使用正则
+    ip = re.findall(r'(?<=\(|\[)\d+\.\d+\.\d+\.\d+(?=\(|\])', text)
+    if os_name == 'nt':
+        time = re.findall(r'\d+(?=ms$)', text)
+    else:
+        time = re.findall(r'(?<=\d/)[\d\.]+(?=/)', text)
+    lost = re.findall(r'\d+(?=%)', text)
+    ip = ip[0] if ip else '0.0.0.0'
+    time = int(time[0]) if time else 0
+    lost = int(lost[0]) if lost else 0
     return host, ip, time, lost
 
 
@@ -61,11 +68,14 @@ if __name__ == '__main__':
     filename = 'hosts.txt'
     add = False
     ips = list()
+    #  包含参数
     if leng >= 2:
         name = argvs[1]
+        # 第一个参数是个文件
         if os.path.isfile(name):
             filename = name
             if leng > 2:
+                # 第二个参数是 +
                 if argvs[2] == '+':
                     add = True
                     ips = argvs[3:]
@@ -77,34 +87,40 @@ if __name__ == '__main__':
                 ips = argvs[2:]
             else:
                 ips = argvs[1:]
+        # 处理 ip/域名参数
         if ips:
             for s in ips:
                 name = s.strip('.,/')
                 name = re.sub(r'https?://', '', name)
                 hosts.append(name)
+    # 既没有 ip/域名参数，也没有文件参数同时默认文件也不存在
     if not hosts and not os.path.isfile(filename):
-        sys.exit('No ip or the file("%s") not existed!' % (filename))
+        sys.exit('No ip/host or the file("%s") not existed!' % (filename))
     if not hosts:
         hosts = get_hosts(filename)
-    if add:
-        hosts = list(set(get_hosts(filename) + hosts))
+    else:
+        # 如果包含 + 参数，合并 ip/域名信息
+        if add:
+            hosts = list(set(get_hosts(filename) + hosts))
     if not hosts:
         sys.exit('Not find ip/host')
     result_time = dict()
     print '#' * 50
+    # 固定字符串长度
     print 'host(ip)'.rjust(33), 'time    lost'.rjust(14)
     for x in hosts:
         host, ip, time, lost = ping(x)
         result_time.update({host: time})
+        if time == 0:
+            lost = 100
         print ('%s(%s): ' % (host, ip)).rjust(35), ('% 3sms   % 2s%%'
                                                     ) % (time, lost)
     times = sorted(result_time.itervalues())
+    # 去除 time 为0的
     times = [i for i in times[:] if i]
     print '#' * 50
     if times:
         for k, v in result_time.iteritems():
             if v == times[0]:
                 print '%s has the min ping time: %s ms' % (k, v)
-    # else:
-        # print 'The ping result are timeout'
     raw_input('>')
